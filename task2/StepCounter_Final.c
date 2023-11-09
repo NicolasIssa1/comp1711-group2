00e6affa99771acf92a8e7a5abb9f4b6fa585f3f
NicolasIssa1 <112622885+NicolasIssa1@users.noreply.github.com>
2023-11-09 20:35:52 +0000

    task2

--- task2/StepCounter_Final.c
+++ task2/StepCounter_Final.c
@@ -6,12 +6,13 @@
 // Struct moved to header file
 
 // Define any additional variables here
-// Global variables for filename and FITNESS_DATA array
-
+// Global variables for filename and FITNESS_DATA array to store the data
+FITNESS_DATA FitnessDataArray[100];
+int count_record = 0;
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
-// Ouputs: date character array; time character array; steps character array
+// Outputs: date character array; time character array; steps character array
 void tokeniseRecord(const char *input, const char *delimiter,
                     char *date, char *time, char *steps) {
     // Create a copy of the input string as strtok modifies the string
@@ -19,7 +20,8 @@ void tokeniseRecord(const char *input, const char *delimiter,
     
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+    if (token != NULL) {
+        strcpy(date, token);
     }
     
     token = strtok(NULL, delimiter);
@@ -34,22 +36,77 @@ void tokeniseRecord(const char *input, const char *delimiter,
     
     // Free the duplicated string
     free(inputCopy);
-
-                    }
-
-
-
-
-// Complete the main function
-int main() {
-   
 }
 
+//This is a function to read data from the file and populate FitnessDataArray ( task 1)
+void readFitnessData(const char* filename, FITNESS_DATA* FitnessDataArray, int* count_record)
+{
+    FILE* file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("Error: Could not find or open the file. \n");
+        //The program returns 1 and exist as said in the note ( because the file cannot be opened succesfully):
+        exit(1);
+    }
 
+    char line_buffer[150];
+    while (fgets(line_buffer, sizeof(line_buffer), file) != NULL)
+     {
+        char date[11];
+        char time[6];
+        char steps_str[15];
 
+        //Then i use the helper function from the header file to tokenise the record
+        tokeniseRecord(line_buffer, ",", date, time, steps_str);
 
+        int steps = atoi(steps_str);
+        strcpy(FitnessDataArray[*count_record].date, date);
+        strcpy(FitnessDataArray[*count_record].time, time);
+        FitnessDataArray[*count_record].steps = steps;
 
+        (*count_record)++;
+     }
+     fclose(file);
+     printf("File successfully loaded. \n");
+}
 
+void displayTotalRecords(int count_record)
+{
+    printf("Total records: %d\n", count_record);
+}
+// Complete the main function
+int main()
+{
+    FITNESS_DATA FitnessDataArray[100];
+    int count_record = 0;
+    char choice;
+    char filename[256];
 
+    printf("\nMenu Options: \n");
+    printf("A: Specify the filename to be imported\n");
+    printf("Q: Quit \n");
+    printf("Enter choice: ");
+    //i used a space before %c to avoid any whitespace 
+    scanf(" %c", &choice);
 
+    switch(choice)
+    {
+        case 'A':
+            printf("Input filename: ");
+            scanf("%s", filename);
+            readFitnessData(filename, FitnessDataArray, &count_record);
+            break;
 
+        case 'B':
+            displayTotalRecords(count_record);
+            break;
+            
+        case 'Q':
+            printf("Program will exit. \n");
+            break;
+        default:
+            printf("Invalid choice. Try again.\n");
+            break;
+    }  
+    return 0;
+}
