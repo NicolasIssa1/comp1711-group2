c51840dfb14acc663bcbbc48522bac60181353e8
NicolasIssa1 <112622885+NicolasIssa1@users.noreply.github.com>
2023-11-28 14:31:15 +0000

    week9

---
+++ week9/pointer_array_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int vector[5] = {1,2,3,4,5};
+
+    int* address;
+
+    int b;
+    
+    address = &b;
+
+    address = vector; 
+
+    printf("\n \n address = %p\n", address);
+
+    printf( " vector = %p\n\n",vector);
+
+    printf ("address[0] = %d  ,  vector[0] = %d  \n\n",address[0],vector[0]);
+
+}
